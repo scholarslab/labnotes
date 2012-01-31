c7ea826dcae210259dccd5165d594a4e08b39529
Jeremy Boggs <jeremy@clioweb.org>
2012-01-31 13:53:17 -0500

    REALLY fixes line ending nonsense.
    Adds content and styles for author links on author profile page.

--- author.php
+++ author.php
@@ -7,9 +7,13 @@ get_header();
 ?>
 
   <h1><?php echo $currentAuthor->first_name; ?> <?php echo $currentAuthor->last_name; ?></h1>
-
+  <div class="author-meta">
   <?php echo get_avatar($currentAuthor->ID, 150); ?>
-
+  <ul>
+        <?php if ($twitter = get_the_author_meta('twitter', $currentAuthor->ID)): ?><li>Twitter: <a href="http://twitter.com/<?php echo $twitter; ?>">@<?php echo $twitter; ?></a></li><?php endif; ?>
+        <?php if ($url = get_the_author_meta('user_url', $currentAuthor->ID)): ?><li>Site: <a href="<?php echo $url; ?>"><?php echo $authorInfo->user_firstname; ?>&#8217;s Site</a></li><?php endif; ?>
+  </ul>
+  </div>
   <div class="entry-content">
 
   <?php echo wpautop($currentAuthor->description); ?>
