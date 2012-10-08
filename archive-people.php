abfb866975a7e427dc94893b1f1c435bad9ed9df
Jeremy Boggs <jeremy@clioweb.org>
2012-10-08 15:53:03 -0400

    Adds code to display graduate fellows.
    Grad fellows are displayed at /people/?category=graduate_fellows and are
    listed alphabetically.
    
    Updates styles to make some parts of staff and fellows listings similar.

--- archive-people.php
+++ archive-people.php
@@ -1,10 +1,58 @@
 <?php get_header(); ?>
-<h1>Staff</h1>
+<?php
+$title = 'Staff';
+if (@$_GET['category'] == 'graduate_fellow') {
+  $title = 'Graduate Fellows';
+}
+?>
+
+  <h1><?php echo $title; ?></h1>
 <div style="overflow:hidden;">
 <?php
 
 // Get the current query.
 global $wp_query;
+
+if (@$_GET['category'] == 'graduate_fellow'):
+
+$fellowArgs = array(
+  'posts_per_page' => '-1',
+  'meta_query' => array(
+    array(
+      'key' => 'person_category',
+      'value' => 'graduate_fellow'
+    )
+  ),
+  'meta_key' => 'person_family_name',
+  'orderby' => 'meta_value',
+  'order' => 'asc'
+);
+
+// Merge the wp_query and $staff args into our defaults.
+$args = array_merge($wp_query->query, $fellowArgs);
+
+query_posts($args);
+if (have_posts()) : ?>
+
+<ul class="people graduate_fellows">
+<?php while (have_posts()) : the_post(); ?>
+<?php $customFields = get_post_custom(); ?>
+  <li class="vcard">
+    <?php echo get_avatar($customFields['person_email'][0],120); ?></a> 
+    <h2 class="fn"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php if ($title = str_replace(' | ', '<br>',$customFields['person_title'][0])): ?>
+    <p class="title"><?php echo $title; ?></p>
+    <?php endif; ?>
+  </li>
+<?php endwhile; ?>
+</ul>
+<?php endif; ?>
+<?php wp_reset_query(); ?>
+
+
+<?php
+
+else:
 
 /**
  * An array of additional arguments we want to pass to the query.
@@ -38,7 +86,7 @@ $defaultArgs = array_merge($wp_query->query, $staffArgs);
  */
 
 // Get an array of our departments and loop through them.
-$departments = array(array('administration' => 'Administration', 'its_research' => 'ITS Research Computing'), array('reseach_and_development' => 'Research & Development'), array('public_service' => 'Outreach & Public Service', 'gis_data' => 'Geospatial Information & Data Services'));
+$departments = array(array('administration' => 'Administration', 'its_research' => 'ITS Research Computing'), array('reseach_and_development' => 'Research & Development', 'gis_data' => 'Geospatial Information & Data Services'), array('public_service' => 'Outreach & Public Service'));
 ?>
 
 <?php foreach ($departments as $departmentArray):?>
@@ -64,13 +112,13 @@ $args = array_merge($defaultArgs, $deptArgs);
 query_posts($args);
 if (have_posts()) : ?>
 <h2><?php echo $title; ?></h2>
-<ul class="staff">
+<ul class="people staff">
 <?php while (have_posts()) : the_post(); ?>
 <?php $customFields = get_post_custom(); ?>
   <li class="vcard">
     <?php echo get_avatar($customFields['person_email'][0],120); ?></a> 
     <h3 class="fn"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-    <?php if ($title = $customFields['person_title'][0]): ?>
+    <?php if ($title = str_replace(' | ', '<br>',$customFields['person_title'][0])): ?>
     <p class="title"><?php echo $title; ?></p>
     <?php endif; ?>
     <ul>
@@ -81,7 +129,7 @@ if (have_posts()) : ?>
     <li class="tel"><?php echo labnotes_format_phone($phone); ?></li>
     <?php endif; ?>
     <?php if ($twitter = $customFields['person_twitter'][0]): ?>
-    <li class="twitter"><a href="http://twitter.com/<?php echo $twitter; ?>" class="url"><?php echo '@'.$twitter; ?></a></li>
+    <li class="twitter"><a href="http://twitter.com/<?php echo str_replace('@', '', $twitter); ?>" class="url"><?php echo '@'.$twitter; ?></a></li>
     <?php endif; ?>
 
     </ul>
@@ -93,5 +141,6 @@ if (have_posts()) : ?>
 <?php endforeach; ?>
 </div>
 <?php endforeach; ?>
+<?php endif; ?>
 </div>
 <?php get_footer(); ?>
