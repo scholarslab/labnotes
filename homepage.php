b0af2b29486c8eceaac115082be094a17d0b1442
Jeremy Boggs <jeremy@clioweb.org>
2014-08-15 16:01:40 -0400

    MOAR TEXT.

--- homepage.php
+++ homepage.php
@@ -19,19 +19,19 @@
 <div id="areas-of-focus">
 <section id="research">
     <h1>Research</h1>
-    <p>Lorem ipsum dolor sit amet.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Illud dico, ea, quae dicat, praeclare inter se cohaerere. Non quaeritur autem quid naturae tuae consentaneum sit, sed quid disciplinae.</p>
 </section>
 <section id="fellowships">
     <h1>Fellowships</h1>
-    <p>Lorem ipsum dolor sit amet.</p>
+<p>Illud non continuo, ut aeque incontentae. Hoc loco tenere se Triarius non potuit. Paupertas si malum est, mendicus beatus esse nemo potest, quamvis sit sapiens.</p>
 </section>
 <section id="makerspace">
     <h1>Makerspace</h1>
-    <p>Lorem ipsum dolor sit amet.</p>
+<p>Recte, inquit, intellegis. Quo modo autem optimum, si bonum praeterea nullum est? Compensabatur, inquit, cum summis doloribus laetitia. Ita relinquet duas, de quibus etiam atque etiam consideret.</p>
 </section>
 <section id="events">
     <h1>Events</h1>
-    <p>Lorem ipsum dolor sit amet.</p>
+<p>Sic, et quidem diligentius saepiusque ista loquemur inter nos agemusque communiter. Nihil sane. Nihil enim hoc differt. Omnia contraria, quos etiam insanos esse vultis. Graccho, eius fere, aequalí?</p>
 </section>
 </div>
 
