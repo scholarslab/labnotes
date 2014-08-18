d7329202e563270a4d51e61af55d1eba1aa680d4
Jeremy Boggs <jeremy@clioweb.org>
2014-08-18 11:28:59 -0400

    Rearranges homepage stuff.

--- homepage.php
+++ homepage.php
@@ -11,28 +11,31 @@
 <section id="homepage-blurb">
 <h1><?php the_title(); ?></h1>
 <?php the_content(); ?>
-<?php dynamic_sidebar( 'homepage-widget-area' ); ?>
-</section>
 
 <?php endwhile; endif; ?>
+</section>
 
-<div id="areas-of-focus">
-<section id="research">
-    <h1>Research</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Illud dico, ea, quae dicat, praeclare inter se cohaerere. Non quaeritur autem quid naturae tuae consentaneum sit, sed quid disciplinae.</p>
-</section>
-<section id="fellowships">
-    <h1>Fellowships</h1>
-<p>Illud non continuo, ut aeque incontentae. Hoc loco tenere se Triarius non potuit. Paupertas si malum est, mendicus beatus esse nemo potest, quamvis sit sapiens.</p>
-</section>
-<section id="makerspace">
-    <h1>Makerspace</h1>
-<p>Recte, inquit, intellegis. Quo modo autem optimum, si bonum praeterea nullum est? Compensabatur, inquit, cum summis doloribus laetitia. Ita relinquet duas, de quibus etiam atque etiam consideret.</p>
-</section>
-<section id="events">
-    <h1>Events</h1>
-<p>Sic, et quidem diligentius saepiusque ista loquemur inter nos agemusque communiter. Nihil sane. Nihil enim hoc differt. Omnia contraria, quos etiam insanos esse vultis. Graccho, eius fere, aequalí?</p>
-</section>
-</div>
+<?php dynamic_sidebar( 'homepage-widget-area' ); ?>
 
+<section id="latest-writing">
+    <h1>Latest Posts</h1>
+<?php
+global $post;
+$args = array( 'posts_per_page' => 3);
+$myposts = get_posts( $args );
+foreach ( $myposts as $post ) : 
+  setup_postdata( $post ); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+            <p class="post-meta">By <?php the_author(); ?> &middot; <?php the_time('F j, Y'); ?> &middot; <?php the_category(', '); ?></p>
+
+        <div class="excerpt">
+        <?php the_excerpt(); ?>
+        </div>
+    </article>
+<?php endforeach;
+wp_reset_postdata(); ?>
+
+
+</section>
 <?php get_footer(); ?>
