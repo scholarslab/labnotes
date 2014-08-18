5a4139fc7b235ed2fd9f402cb87fc7d196ddf44f
Jeremy Boggs <jeremy@clioweb.org>
2014-08-18 14:40:44 -0400

    Ongoing work displaying people.

--- archive-people.php
+++ archive-people.php
@@ -7,20 +7,77 @@ $defaultArgs = array('post_type' => 'people', 'posts_per_page' => -1, 'meta_key'
 // Merge the  and $staff args into our defaults.
 $staffArgs = array_merge($defaultArgs, array('people-category' => 'staff'));
 
+$studentAssistantArgs = array_merge($defaultArgs, array('people-category' => 'student-assistant'));
+
+$gradFellowArgs = array_merge($defaultArgs, array('people-category' => 'graduate-fellow, praxis-fellow'));
+
+$praxisFellowArgs = array_merge($defaultArgs, array('people-category' => 'praxis-fellow'));
+
 ?>
+
 <header>
     <h1>People</h1>
 </header>
 
 <?php
-query_posts($staffArgs);
+
+$current = array(
+    'meta_query' => array(
+        array(
+        'key'=>'person_status',
+        'value'=> 'current',
+        'compare' => '='
+        )
+    )
+);
+
+$currentStaffArgs = array_merge($staffArgs, $current);
+
+query_posts($currentStaffArgs);
 if (have_posts()) : ?>
 <h2><?php echo single_cat_title(); ?></h2>
-<ul class="people-list">
+<ul class="people-list current">
 <?php while( have_posts() ) : the_post(); ?>
 <li>
 <a href="<?php the_permalink(); ?>">
     <img src="<?php echo labnotes_people_image(); ?>" alt="" />
+    <?php the_title(); ?>
+</a>
+</li>
+<?php endwhile; ?>
+</ul>
+<?php endif; ?>
+<?php wp_reset_query(); ?>
+
+<?php
+
+$currentStudentArgs = array_merge($studentAssistantArgs, $current);
+    
+query_posts($currentStudentArgs);
+if (have_posts()) : ?>
+<h3>Student Assistants</h3>
+<ul class="people-list current">
+<?php while( have_posts() ) : the_post(); ?>
+<li>
+<a href="<?php the_permalink(); ?>">
+    <img src="<?php echo labnotes_people_image(); ?>" alt="" />
+    <?php the_title(); ?>
+</a>
+</li>
+<?php endwhile; ?>
+</ul>
+<?php endif; ?>
+<?php wp_reset_query(); ?>
+
+<?php
+
+query_posts($gradFellowArgs);
+if (have_posts()) : ?>
+<h2>Graduate Fellows</h2>
+<ul class="people-list">
+<?php while( have_posts() ) : the_post(); ?>
+<li>
+<a href="<?php the_permalink(); ?>">
     <?php the_title(); ?>
 </a>
 </li>
