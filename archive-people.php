49f2d913e637d8281275ef5a44ae79be8a8609f5
Jeremy Boggs <jeremy@clioweb.org>
2014-08-19 11:43:33 -0400

    Simplifies this, I guess. Who knows at this point.

--- archive-people.php
+++ archive-people.php
@@ -4,36 +4,49 @@
 
 $defaultArgs = array('post_type' => 'people', 'posts_per_page' => -1, 'meta_key' => 'person_family_name', 'orderby' => 'meta_value', 'order' => 'asc');
 
-// Merge the  and $staff args into our defaults.
-$staffArgs = array_merge($defaultArgs, array('people-category' => 'staff'));
-
-$studentAssistantArgs = array_merge($defaultArgs, array('people-category' => 'student-assistant'));
-
-$currentFellowArgs = array_merge($defaultArgs, array('people-category' => '2014-2015-praxis-fellow, 2014-2015'));
+$currentCategories = array(
+    'staff' => 'Staff',
+    'student-assistant' => 'Student Assistants',
+    '2014-2015-praxis-fellow, 2014-2015' => 'Gradute Fellows'
+);
 
 ?>
-
 <header>
     <h1>People</h1>
 </header>
 
 <?php
 
-$current = array(
-    'meta_query' => array(
-        array(
-        'key'=>'person_status',
-        'value'=> 'current',
-        'compare' => '='
+foreach ($currentCategories as $category => $title) :
+
+    $currentArgs = array(
+        'people-category' => $category,
+        'meta_query' => array(
+            array(
+                'key'=>'person_status',
+                'value'=> 'current',
+                'compare' => '='
+            )
         )
-    )
-);
+    );
 
-$currentStaffArgs = array_merge($staffArgs, $current);
+    $currentArgs = array_merge($defaultArgs, $currentArgs);
 
-query_posts($currentStaffArgs);
+query_posts($currentArgs);
 if (have_posts()) : ?>
-<h2><?php echo single_cat_title(); ?></h2>
+
+<?php
+    $headingTag = 'h2';
+
+    if ($category == 'student-assistant') {
+        $headingTag = 'h3';
+    }
+
+    $heading = '<'.$headingTag.'>'
+           .$title.'</'.$headingTag.'>';
+
+    echo $heading;
+?>
 <ul class="people-list current">
 <?php while( have_posts() ) : the_post(); ?>
 <li>
@@ -44,56 +57,15 @@ if (have_posts()) : ?>
 </li>
 <?php endwhile; ?>
 </ul>
-<?php endif; ?>
-<?php wp_reset_query(); ?>
+<?php endif; wp_reset_query(); endforeach; ?>
 
-<?php
-
-$currentStudentArgs = array_merge($studentAssistantArgs, $current);
-    
-query_posts($currentStudentArgs);
-if (have_posts()) : ?>
-<h3>Student Assistants</h3>
-<ul class="people-list current">
-<?php while( have_posts() ) : the_post(); ?>
-<li>
-<a href="<?php the_permalink(); ?>">
-    <img src="<?php echo labnotes_custom_background_image_src(); ?>" alt="" />
-    <?php the_title(); ?>
-</a>
-</li>
-<?php endwhile; ?>
-</ul>
-<?php endif; ?>
-<?php wp_reset_query(); ?>
-
-<?php
-
-query_posts($currentFellowArgs);
-if (have_posts()) : ?>
-<h2>Current Fellows</h2>
-<ul class="people-list">
-<?php while( have_posts() ) : the_post(); ?>
-<li>
-<a href="<?php the_permalink(); ?>">
-    <img src="<?php echo labnotes_custom_background_image_src(); ?>" alt="" />
-    <?php the_title(); ?>
-</a>
-</li>
-<?php endwhile; ?>
-</ul>
-<?php endif; ?>
-<?php wp_reset_query(); ?>
-
-<?php
-
-
-if (have_posts()) : ?>
+<?php if (have_posts()) : ?>
 <h2>Alumni</h2>
 <ul class="people-list">
 <?php while( have_posts() ) : the_post(); ?>
 <?php
-if(is_null($status) || $status == 'current' || has_term(array('2014-2015', '2014-2015-praxis-fellow'), 'people-category')) continue;
+     $status = get_post_meta( $post->ID, 'person_status', true);
+if(is_null($status) || $status == 'current') continue;
 ?>
 <li>
 <a href="<?php the_permalink(); ?>">
