1dee9fb4b7c834062a46169dcddfce41e6ac3470
Jeremy Boggs <jeremy@clioweb.org>
2014-08-27 09:50:09 -0400

    Sorts research by category. Refs #27.
    Instead of querying for categories, I use an array of slugs to order these the
    way I want. Not flexible.

--- archive-research.php
+++ archive-research.php
@@ -1,17 +1,47 @@
 <?php get_header(); ?>
+
+<?php
+
+$defaultArgs = array('post_type' => 'research', 'posts_per_page' => -1);
+
+$researchCategories = array(
+    'scholars-lab',
+    'collaboration'
+);
+
+?>
+
 <header>
     <h1><?php _e( 'Research', 'labnotes' ); ?></h1>
 </header>
-<?php if ( have_posts() ) : ?>
+
+<?php
+
+foreach ($researchCategories as $category):
+
+$categoryArgs = array(
+    'research-category' => $category
+);
+
+$args = array_merge($defaultArgs, $categoryArgs);
+
+query_posts($args);
+
+if ( have_posts() ) : 
+
+?>
+<h2><?php single_cat_title(); ?></h2>
+<div class="category-description"><?php echo category_description(); ?></div>
 <ul class="research">
 <?php while (have_posts()) : the_post(); ?>
 <li>
     <a href="<?php the_permalink(); ?>" class="permalink">
-    <h2><?php the_title(); ?></h2>
     <?php the_post_thumbnail('full'); ?>
+    <h2><?php the_title(); ?></h2>
     </a>
 </li>
 <?php endwhile; ?>
 </ul>
-<?php endif; ?>
+<?php endif; wp_reset_query(); endforeach; ?>
+
 <?php get_footer(); ?>
